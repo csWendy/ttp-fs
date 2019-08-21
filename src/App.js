fc4740f5ec86ebf46a85e9b3dc67c59461cdbcb5
Wendy <wendy.wang55@gmail.com>
2019-08-21 19:07:23 -0400

    work on App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import Register from './Components/Register';
+import Navigation from "./Components/Navigation/Navigation";
+import Landing from "./Components/Landing/Landing";
+// import { directive } from '@babel/types';
+
+const App=() => (
+  <div>
+    <BrowserRouter>
+      <Navigation/>
+      <Switch>
+        <Route path ="/" component ={Landing} exact/>
+        <Route exact path="register" component={Register}/>
+        </Switch>
+    </BrowserRouter>
     </div>
-  );
-}
+)
 
 export default App;
